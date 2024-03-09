3e64881ab7f0dfd3aebd0bfce5437e8194c02188
bordjos <bbordjoski97@gmail.com>
2024-03-09 14:26:05 +0100

    End of section 3

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  pictureUrl: string;
+  type?: string;
+  brand: string;
+  quantityInStock?: number;
+  // adding the ? makes those properties optional
+}
